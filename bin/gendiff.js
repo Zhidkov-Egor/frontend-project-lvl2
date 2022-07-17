cd2d1e15b9549144c593958c2056865a779c7bf8
Egor Zhidkov <e.zhidkov@idaproject.com>
2022-07-17 14:03:03 +0300

    add option and argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,7 @@ const program = new Command();
 program
   .description('Comparing two files')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .parse(process.argv);
